7a77ad629ff770bcf333cda0499310761758f375
craig wadding <craig.wadding@gmail.com>
2019-03-23 14:01:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,7 +6,7 @@ def array_with_two_elements
   ["one", "two"] 
 end
 
-def my_first_element
+def my_first_element(first_element)
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 puts taylor_swift[0] 
   
