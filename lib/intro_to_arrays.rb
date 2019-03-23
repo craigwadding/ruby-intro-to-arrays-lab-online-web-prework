e21cd78ae2c18d29bdea88c7c3947e23a5c6c7ab
craig wadding <craig.wadding@gmail.com>
2019-03-23 13:16:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,7 +6,7 @@ def array_with_two_elements
   ["one", "two"] 
 end
 
-def my_first_element
+def first_element
  Taylor Swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-  my_first_element.first 
+  first_element.first 
 end
